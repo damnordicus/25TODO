8c39ccea850b83e406358da096d16eeeb66e3b74
nord <adam.w.nord@gmail.com>
2024-05-14 12:52:26 -0700

    completed rquirements, working on localStorage

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let todoList = []; // global array for the ToDo list
+let completedItems = []; // global array for completed tasks
 
 function addItemToList (item) {
     // Takes in a new item "item" as a string and adds it to the end of the List
@@ -8,13 +9,34 @@ function addItemToList (item) {
 } 
 
 function addElement (list){
+    // if this is the first item being added to the main list
+    if(list.length === 1){
+        // create the instructions 
+        const instruct = document.createElement("h2");
+        instruct.textContent= "Click the item to delete it!";
+        instruct.classList.add("h2");
+        instruct.id = "instruction";
+        document.getElementById("itemList").appendChild(instruct);
+    }
     // establishes a newItem that is a button
     const newItem = document.createElement("button");
     // makes the text for the button whatever the last items is on the list
     const buttonText =  document.createTextNode(list[list.length - 1]);
     // adds an eventListener to each new button to allow you to delete bu clicking it
     newItem.addEventListener("click", function() {
+        // add the text from the button to the completed array
+        completedItems.push(newItem.innerHTML);
+        // add the same item "text" to the completed list view
+        updateCompletedContainer(newItem.innerHTML);
+        // remove item from list
         newItem.parentNode.removeChild(newItem);
+        // remove item from the array
+        list.pop();
+        // if there are no more tasks, delete the instructions
+        if(list.length === 0){
+            const instruct = document.getElementById("instruction");
+            instruct.parentNode.removeChild(instruct);
+        }
     })
     // adds the text from the list to the button
     newItem.appendChild(buttonText);
@@ -24,10 +46,30 @@ function addElement (list){
 
 function submitHandle(event){
     event.preventDefault();
+
+    if(document.getElementById("newItem").value === ""){
+        alert("Enter a new task!");
+        return;
+    }
     // takes the input text from the textbox and passes it to
     // addItemToList as a string
     addItemToList(document.getElementById("newItem").value);
+    document.getElementById("newItem").value = "";
 }
 
 // event listener for the submit button to handle a click
 document.getElementById("addItemButton").addEventListener("click", submitHandle);
+// event listener for pressing the enter key
+document.getElementById("newItem").addEventListener("keydown", function(e){
+    if(e.key === "Enter"){
+       submitHandle(e);
+    }
+})
+
+function updateCompletedContainer(item){
+    const newItem = document.createElement("li");
+    // makes the text for the button whatever the last items is on the list
+    newItem.textContent = item;
+    // adds the button to the div list
+    document.getElementById("completed-container").appendChild(newItem);
+}
